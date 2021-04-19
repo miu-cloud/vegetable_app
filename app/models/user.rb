34d0e6a405049eb866a373b3c42e1d575dfc3951
miu-cloud <76993219+miu-cloud@users.noreply.github.com>
2021-04-19 15:39:50 +0900

    Revert "Issues/#1"

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, presence: true
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-end
