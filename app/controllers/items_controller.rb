9cea91b1910ddaa83319186bb51c279b76b76028
miu oohira <miu.000408@gmail.com>
2021-04-20 17:31:22 +0900

    投稿したユーザの詳細画面作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,25 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    @item.save
+    redirect_to item_path(@item)
   end
 
   def edit
+  end
+ 
+  private
+  def item_params
+    params.require(:item).permit(:title, :content, :count ,:place, :promise_at, :state, :image)
   end
 end
