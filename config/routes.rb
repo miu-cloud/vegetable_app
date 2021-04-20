bce302527911b1d8e24eb9667817043097c0b468
miu oohira <miu.000408@gmail.com>
2021-04-20 14:35:49 +0900

    itemのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/edit'
   devise_for :users
   # root to: "home#index"
   root 'users#index' 
   resources :users
+  resources :items
 end
