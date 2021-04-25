af207c5e0be075c7bcbcf15005151bea98d720b6
miu oohira <miu.000408@gmail.com>
2021-04-25 15:02:07 +0900

    見た目の調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#top"
   get '/mypage' => 'users#mypage'  #新規登録後orログイン後、マイページへ
+  
   resources :users do
     collection do
       get 'search'
