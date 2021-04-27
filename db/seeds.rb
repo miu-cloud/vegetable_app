d283eac664103dba4aac43f581a04ba7f034ee3e
miu oohira <miu.000408@gmail.com>
2021-04-27 22:09:51 +0900

    seedデータの登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+    User.create!(
+      email: "test#{n + 1}@test.com",
+      name: "田中太郎#{n + 1}",
+      password: "000000"
+    )
+end
+
+5.times do |n|
+    Item.create!(
+      user_id: User.first.id + n, 
+      title: "トマト",
+      content: "規格外品です#{n + 1}",
+      count: 1,
+      place: "公園#{n + 1}",
+      promise_at: Date.today,
+      state: "可",
+      image: File.open('./app/assets/images/トマト.jpeg')
+    )
+end
+
+5.times do |n|
+    Item.create!(
+      user_id: User.ids[5]+ n, 
+      title: "キュウリ",
+      content: "規格外品です#{n + 1}",
+      count: 2,
+      place: "駅前#{n + 1}",
+      promise_at: Date.today.since(1.weeks),
+      state: "可",
+      image: File.open('./app/assets/images/きゅうり.jpeg')
+    )
+end
