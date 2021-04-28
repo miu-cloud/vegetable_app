de5d6ad322f31aae4629d4c2bbb5789bbf40331c
miu oohira <miu.000408@gmail.com>
2021-04-28 11:52:51 +0900

    コントローラでアクセス制御

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#top"
+  root to:"home#top"
   get '/mypage' => 'users#mypage'  #新規登録後orログイン後、マイページへ
   
   resources :users do
